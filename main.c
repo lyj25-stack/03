1e7665ae3afd19c587dcf98046c85e2cf8ed5017
lyj25-stack <lyj25@sookmyung.ac.kr>
2025-09-18 16:21:21 +0900

    03

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(void){
-    int input_int;
-    float input_float;
+int main(void)
+{
+	char c;
+	int i;
+	
+	printf("input a number:");
+    scanf("%c",&c);
     
-    printf("enter an integer : ");
-    scanf("%d",&input_int);
+    i=c-'0';
+    printf("The input number is %i\n",i);
     
-	printf("enter an float : ");
-    scanf("%d",&input_float);
-    
-    printf("integer : %d,float : %f/n,input_int,input_float");
     return 0;
 }
