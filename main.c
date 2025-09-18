0e5165c43b88c67fa5cf5b05a02a76aa04d54dac
lyj25-stack <lyj25@sookmyung.ac.kr>
2025-09-18 16:49:05 +0900

    03

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(int argc,char *argv[]){
-	char c,d;
-	 printf("enter a character :");
-	 scanf("%c",&c);
-	 d=c+1;
-	 printf("The next character of %c (%i)is %c,(%i).\n",c,c,d,d);
-
-     return 0;}
+int main(void){
+    int ja,mo;
+        
+     printf("enter bunja:");
+     scanf("%d",&ja);
+     
+     printf("enter bunmo:");
+     scanf("%d",&mo);
+    
+    printf("The result is %f\n",(float)ja/mo); 
+    return 0;
+}
